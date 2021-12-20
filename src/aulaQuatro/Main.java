9268cf5f3e68052809ad32d1e73efa62fa544c0b
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2021-12-20 11:06:45 -0300

    Criação do arquivo base, classe main

---
+++ src/aulaQuatro/Main.java
@@ -0,0 +1,12 @@
+package aulaQuatro;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Date relogio = new Date();
+        System.out.println(relogio.toString());
+    }
+}
